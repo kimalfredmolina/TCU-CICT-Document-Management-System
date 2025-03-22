5485020f7f3434159c9e01f4a9964eeadf904fef
Kim Alfred A. Molina <119164038+kimalfred@users.noreply.github.com>
2025-03-22 11:43:44 +0800

    Added new page and adjust the sidebar

---
+++ Document Management System/Pages/AdminPage/UserTable.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Document_Management_System.Pages.AdminPage
+{
+    public class UserTableModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
