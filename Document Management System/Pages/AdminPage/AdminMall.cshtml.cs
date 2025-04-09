8f111b8d132e5e00dce3acc8d2b99464199665eb
laurence deocareza <laurence.deocareza@gmail.com>
2025-04-09 08:50:57 +0800

    table responsive to the data in db and a search

--- Document Management System/Pages/AdminPage/AdminMall.cshtml.cs
+++ Document Management System/Pages/AdminPage/AdminMall.cshtml.cs
@@ -1,12 +1,43 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Document_Management_System.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Document_Management_System.Pages.AdminPage
 {
     public class AdminMallModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<Users> _userManager;
+
+        public AdminMallModel(UserManager<Users> userManager)
         {
+            _userManager = userManager;
+        }
+
+        public List<Users> UsersList { get; set; } = new List<Users>();
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchQuery { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var users = _userManager.Users;
+
+            if (!string.IsNullOrEmpty(SearchQuery))
+            {
+                // Filter users based on the search query
+                UsersList = users
+                    .Where(u => u.Id.Contains(SearchQuery) || u.UserName.Contains(SearchQuery) || u.Email.Contains(SearchQuery))
+                    .ToList();
+            }
+            else
+            {
+                // Fetch all users if no search query is provided
+                UsersList = users.ToList();
+            }
         }
     }
 }
