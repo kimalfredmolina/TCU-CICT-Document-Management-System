c72aed0c43c9ab4044d58502954f3ad66c34dd52
laurence deocareza <laurence.deocareza@gmail.com>
2025-04-11 22:59:28 +0800

    added table data for files

--- Document Management System/Data/AppDbContext.cs
+++ Document Management System/Data/AppDbContext.cs
@@ -6,12 +6,26 @@ namespace Document_Management_System.Data
 {
     public class AppDbContext : IdentityDbContext<Users>
     {
-        public AppDbContext(DbContextOptions options) : base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
 
-        protected AppDbContext()
-        {
-        }
+        // Add the missing DbSet for Documents  
+        public DbSet<Document> Documents { get; set; }
     }
+    public class Document
+    {
+        public int Id { get; set; } // Primary Key
+        public string Filename { get; set; } // NOT NULL
+        public long? Filesize { get; set; } // instead of int? nullable
+        public string FileType { get; set; } // NOT NULL
+        public int? CategoryId { get; set; } // Nullable
+        public string UploadedBy { get; set; } // NOT NULL
+        public DateTime? UploadedDate { get; set; } // Nullable
+        public string ContentType { get; set; } // NOT NULL
+        public byte[]? FileData { get; set; } //NULLABLE
+
+    }
+
+
 }
