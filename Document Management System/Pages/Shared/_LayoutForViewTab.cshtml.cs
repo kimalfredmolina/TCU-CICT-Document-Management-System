a6361ac8f4a75af5c8b3557b6e3dc4c4c72f34b7
Kim Alfred A. Molina <119164038+kimalfred@users.noreply.github.com>
2025-04-16 11:40:32 +0800

    add file folder

---
+++ Document Management System/Pages/Shared/_LayoutForViewTab.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Document_Management_System.Pages.Shared
+{
+    public class _LayoutForViewTabModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
