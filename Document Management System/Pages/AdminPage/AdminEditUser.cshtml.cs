2cc322234b1c620829d404090230cd47635989af
Kim Alfred A. Molina <kimalfred22molina@gmail.com>
2025-04-18 21:04:01 +0800

    Can update the name now but theres an error on password not changing it and it require to input

--- Document Management System/Pages/AdminPage/AdminEditUser.cshtml.cs
+++ Document Management System/Pages/AdminPage/AdminEditUser.cshtml.cs
@@ -54,52 +54,27 @@ namespace Document_Management_System.Pages.AdminPage
 
             UserId = id;
 
-            string connectionString = _configuration.GetConnectionString("Default");
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
             {
-                string query = @"SELECT 
-                        u.Id,
-                        u.UserName,
-                        u.Email,
-                        u.PhoneNumber,
-                        p.FullName,
-                        p.ProfileImage
-                        FROM dbo.AspNetUsers u
-                        LEFT JOIN dbo.AspNetUsers p ON u.Id = p.Id
-                        WHERE u.Id = @Id";
+                return NotFound();
+            }
 
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.Add("@Id", System.Data.SqlDbType.NVarChar).Value = id;
+            Input = new InputModel
+            {
+                UserName = user.UserName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber,
+                FullName = user.fullName
+            };
 
-                connection.Open();
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    if (reader.Read())
-                    {
-                        Input = new InputModel
-                        {
-                            UserName = reader["UserName"].ToString(),
-                            Email = reader["Email"].ToString(),
-                            PhoneNumber = reader["PhoneNumber"]?.ToString(),
-                            FullName = reader["FullName"]?.ToString()
-                        };
-
-                        if (reader["ProfileImage"] != DBNull.Value)
-                        {
-                            byte[] imageData = (byte[])reader["ProfileImage"];
-                            UserProfileImage = $"data:image/png;base64,{Convert.ToBase64String(imageData)}";
-                        }
-                        else
-                        {
-                            UserProfileImage = null;
-                        }
-                    }
-                    else
-                    {
-                        return RedirectToPage("/AdminPage/AdminUserTable");
-                    }
-                }
+            if (user.ProfileImage != null)
+            {
+                UserProfileImage = $"data:image/png;base64,{Convert.ToBase64String(user.ProfileImage)}";
+            }
+            else
+            {
+                UserProfileImage = null;
             }
 
             return Page();
@@ -129,8 +104,8 @@ namespace Document_Management_System.Pages.AdminPage
             }
 
             user.PhoneNumber = Input.PhoneNumber;
+            user.fullName = Input.FullName;
 
-            // Update password only if both password and confirm password are provided and match
             if (!string.IsNullOrEmpty(Input.Password) && !string.IsNullOrEmpty(Input.ConfirmPassword))
             {
                 if (Input.Password != Input.ConfirmPassword)
@@ -139,6 +114,7 @@ namespace Document_Management_System.Pages.AdminPage
                     return Page();
                 }
 
+                // Remove the current password
                 var removePasswordResult = await _userManager.RemovePasswordAsync(user);
                 if (!removePasswordResult.Succeeded)
                 {
@@ -149,6 +125,7 @@ namespace Document_Management_System.Pages.AdminPage
                     return Page();
                 }
 
+                // Add the new password
                 var addPasswordResult = await _userManager.AddPasswordAsync(user, Input.Password);
                 if (!addPasswordResult.Succeeded)
                 {
@@ -160,7 +137,7 @@ namespace Document_Management_System.Pages.AdminPage
                 }
             }
 
-            // Handle profile image upload only if a new image is provided
+            // Handle profile image upload
             if (ProfileImage != null && ProfileImage.Length > 0)
             {
                 using (var memoryStream = new MemoryStream())
@@ -170,28 +147,7 @@ namespace Document_Management_System.Pages.AdminPage
                 }
             }
 
-            // Update full name in the database - we'll update regardless of whether it's null
-            string connectionString = _configuration.GetConnectionString("Default");
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                string query = @"UPDATE dbo.AspNetUsers 
-                        SET FullName = @FullName
-                        WHERE Id = @Id";
-
-                SqlCommand command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@FullName", Input.FullName ?? (object)DBNull.Value);
-                command.Parameters.AddWithValue("@Id", id);
-
-                connection.Open();
-                int rowsAffected = command.ExecuteNonQuery();
-
-                if (rowsAffected == 0)
-                {
-                    ModelState.AddModelError(string.Empty, "Failed to update user name.");
-                    return Page();
-                }
-            }
-
+            // Update all user properties at once
             var result = await _userManager.UpdateAsync(user);
             if (!result.Succeeded)
             {
