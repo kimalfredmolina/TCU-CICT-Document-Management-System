c092608bf0e6aca26b5ce6855411a31082da7d45
jp-22-00540 <nel.penaflor12@gmail.com>
2025-04-22 20:30:32 +0800

    ADD FILES UPDATE

--- Document Management System/Pages/AdminPage/AdminFilesAdd.cshtml.cs
+++ Document Management System/Pages/AdminPage/AdminFilesAdd.cshtml.cs
@@ -1,12 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Document_Management_System.Pages.AdminPage
 {
     public class AdminFilesAddModel : PageModel
     {
+        [BindProperty]
+        public string FileName { get; set; }
+
+        [BindProperty]
+        public string Categories { get; set; }
+
+        [BindProperty]
+        public string ContentType { get; set; }
+
+        [BindProperty]
+        public IFormFile FormFile { get; set; }
+
         public void OnGet()
         {
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || FormFile == null)
+            {
+                return Page();
+            }
+
+            // Create uploads directory if it doesn't exist
+            var uploadsDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+            if (!Directory.Exists(uploadsDir))
+            {
+                Directory.CreateDirectory(uploadsDir);
+            }
+
+            // Sanitize filename
+            var fileName = Path.GetFileName(FormFile.FileName);
+            var filePath = Path.Combine(uploadsDir, fileName);
+
+            // Save file
+            using (var stream = System.IO.File.Create(filePath))
+            {
+                await FormFile.CopyToAsync(stream);
+            }
+
+            // Here you would typically save to database
+            // await _documentService.AddDocumentAsync(FileName, Categories, ContentType, filePath);
+
+            return RedirectToPage("./AdminFiles");
+        }
+
     }
 }
